acfa4c301cffba0945b4868f5865b065461e4382
trusov.andrew <trusov.andrew@gmail.com>
2015-12-13 01:32:33 +0400

    Add Main code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,21 @@
+import org.eclipse.jetty.server.Server;
+
 /**
  * Created by user on 13.12.15.
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        Frontend frontend = new Frontend();
 
+        Server server = new Server(8080);
+        ServletContextHandler context =
+                new ServletContextHandler(ServletContextHandler.SESSIONS);
+        server.setHandler(context);
+
+        context.addServlet(new ServletHolder(frontend), '/authform');
+
+        server.start();
+
+        server.join();
     }
 }
