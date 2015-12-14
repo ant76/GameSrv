33465b83874af8fe36301759f3543a1d5cc4db8f
ant76 <trusov.andrew@gmail.com>
2015-12-14 17:05:55 +0400

    Add frontend layers files

---
+++ src/main/java/frontend/Mirror.java
@@ -0,0 +1,28 @@
+package frontend;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by user on 14.12.2015.
+ */
+public class Mirror extends HttpServlet {
+    private String login = "";
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+//        resp.getWriter().println(PageGenerator.getPage("mirror.html", pageVariables));
+//        resp.setContentType("text/html;charset=utf8");
+//        resp.setStatus(HttpServletResponse.SC_OK);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //super.doPost(req, resp);
+    }
+}
