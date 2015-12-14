6e2a41553168323664752c410b473e4199d9af9c
ant76 <trusov.andrew@gmail.com>
2015-12-14 17:48:15 +0400

    fix Mirrror servlet

--- src/main/java/frontend/Mirror.java
+++ src/main/java/frontend/Mirror.java
@@ -1,10 +1,14 @@
 package frontend;
 
+import templater.PageGenerator;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by user on 14.12.2015.
@@ -15,10 +19,11 @@ public class Mirror extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-//        resp.getWriter().println(PageGenerator.getPage("mirror.html", pageVariables));
-//        resp.setContentType("text/html;charset=utf8");
-//        resp.setStatus(HttpServletResponse.SC_OK);
+        Map<String, Object> pageVariables = new HashMap<>();
+        pageVariables.put("value", req.getAttribute("key"));
+        resp.getWriter().println(PageGenerator.getPage("mirror.html", pageVariables));
+        resp.setContentType("text/html;charset=utf8");
+        resp.setStatus(HttpServletResponse.SC_OK);
     }
 
     @Override
