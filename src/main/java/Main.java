f5bea5780c0b150ac4ad91f117fd2b0c7d6d9552
ant76 <trusov.andrew@gmail.com>
2015-12-15 17:01:48 +0400

    add Logger

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,9 +3,14 @@ import frontend.Mirror;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        Logger logger = LoggerFactory.getLogger(Main.class);
+        logger.info("Hello World");
+
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
 
         context.addServlet(new ServletHolder(new Frontend()), "/auth_form");
@@ -17,7 +22,12 @@ public class Main {
 
 
         server.start();
-        System.out.println("Server started");
+        logger.info("Server started");
+//        logger.trace("Hello World");
+//        logger.debug("Hello World");
+//        logger.info("Hello World");
+//        logger.warn("Hello World");
+//       logger.error("Hello World");
 
         server.join();
     }
